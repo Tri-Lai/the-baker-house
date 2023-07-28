db8f80be810b3926fdb861985ed3108cface8696
Iris Lai <s3799602@rmit.edu.vn>
2023-07-28 12:51:16 +0700

    Add colour scheme models.

---
+++ the baker house/Views/Shop List/ShopList.swift
@@ -0,0 +1,24 @@
+//
+//  ShopList.swift
+//  the baker house
+//
+//  Created by Trí Lai on 28/07/2023.
+//
+
+import SwiftUI
+
+struct ShopList: View {
+    var body: some View {
+        ZStack {
+            
+        }
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopList_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopList()
+    }
+}
