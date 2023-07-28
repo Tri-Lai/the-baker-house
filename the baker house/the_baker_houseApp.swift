c8ad0a6a936641c7d9241084dfe0392105bd6963
Iris Lai <s3799602@rmit.edu.vn>
2023-07-28 12:07:23 +0700

    Initial Commit

---
+++ the baker house/the_baker_houseApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_baker_houseApp.swift
+//  the baker house
+//
+//  Created by Trí Lai on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct the_baker_houseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
