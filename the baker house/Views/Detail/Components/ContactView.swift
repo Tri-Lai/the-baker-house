079c744de71c34123aee909ed90bd928d9e164f2
Iris Lai <s3799602@rmit.edu.vn>
2023-08-07 16:30:38 +0700

    Add header acknowledgement.

--- the baker house/Views/Detail/Components/ContactView.swift
+++ the baker house/Views/Detail/Components/ContactView.swift
@@ -5,6 +5,18 @@
 //  Created by Trí Lai on 03/08/2023.
 //
 
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 1
+  Author: Lai Nghiep Tri
+  ID: s3799602
+  Created  August 7th, 2023
+  Last modified: August 7th, 2023
+  Acknowledgement: iOS course COSC2659 (lectures and tutorials), Apple Documentation
+*/
+
 import SwiftUI
 
 struct ContactView: View {
